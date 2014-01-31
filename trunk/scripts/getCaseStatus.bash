e10c516dcc3b5d9f129f305a2deaa214432afb7b
xylarstorm@gmail.com <xylarstorm@gmail.com@0952e5f2-6538-abc9-d85b-31e1e9755ba0>
2014-01-31 13:31:38 +0000

    getCaseStatus.bash now lets the user know if a run failed (either blew up or the inner loop did not converge) so it can be re-run with a smaller step.

--- trunk/scripts/getCaseStatus.bash
+++ trunk/scripts/getCaseStatus.bash
@@ -11,13 +11,25 @@ do
   expt=${var}_${value}
   prefix=$pFolder/$vFolder/$expt
   if [[ -f ${prefix}_final.log ]] ; then
+    logFile=${prefix}_final.log
     echo $prefix: final
   elif [[ -f ${prefix}_strict.log ]] ; then
+    logFile=${prefix}_strict.log
     echo $prefix: strict
   elif [[ -f ${prefix}_loose.log ]] ; then
+    logFile=${prefix}_loose.log
     echo $prefix: loose
   else
+    logFile=${prefix}_init.log
     echo $prefix: init
+  fi
+  result=`grep blew "$logFile"`
+  if [[ -n $result ]] ; then
+    echo failed! Try reducing CFL.
+  fi
+  result=`grep Eror "$logFile"`
+  if [[ -n $result ]] ; then
+    echo failed! Try reducing CFL.
   fi
 
 done
