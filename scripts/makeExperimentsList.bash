1e5237dd0a736076c77e080f54d89b7b5f7aee87
xylarstorm <xylarstorm@gmail.com>
2014-02-07 15:21:47 +0000

    A new way of submitting jobs.
    First, one time run the following script in the root directory containing the directories for each p case:
    > code/scripts/makeExperimentsList.bash
    This will generate experiments.txt, all the info for all "vary" experiments.
    Then, each time you want to submit a new set of jobs, run:
    > code/scripts/makeExperimentSets.bash
    > code/scripts/submitJobs.bash
    The first script will go through the full list of experiments, ignoring those that have finished, and put them in separate sets of 24 in experiments1.txt, etc.
    The second script will submit one job each for experiment sets 1, 2, 3, etc. in experiments1.txt, experiments2.txt, etc.

--- scripts/makeExperimentsList.bash
+++ scripts/makeExperimentsList.bash
@@ -1,96 +1,86 @@
 #!/bin/bash
-#PBS -q regular
-#PBS -l mppwidth=24
-#PBS -l walltime=12:00:00
-#PBS -N touchstone
-#PBS -A m1041
-#PBS -m ae
 
-cd $PBS_O_WORKDIR
+fileName=experiments.txt
+rm $fileName
+for p in 0.00 0.25 0.50 0.75 1.00
+do
 
-module load python
-
-if [[ -z $p ]] ; then
-  echo "please set p by running \'qsub -v p=x.xx ...\'"
-  exit 1
-fi
-
-echo p=$p 
 slope_ref=778.5
 C_ref=7.624e6
 lambda_ref=2.0
 A_ref=4.6416e-25
-codeFolder=../../code
-CFL=400
 dt=1e-6
 maxXg=100.0
 minXg=0.6936416184971098
 
-mkdir -p vary_A
-cd vary_A
+CFL=200
 for A in 4.6416e-24 2.1544e-24 1.0000e-24 4.6416e-25 2.1544e-25 1.0000e-25 \
   4.6416e-26 2.1544e-26 1.0000e-26
 do
   prefix=A_$A
   commonArgs="--folder=. --p=$p --A=$A --linearSlope=$slope_ref --C=$C_ref --lambda_0=$lambda_ref --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  echo p_$p/vary_A >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
-cd ..
 
-mkdir -p vary_C
-cd vary_C
-C_CFL=50
+CFL=50
 for C in 3.812e8 7.624e7 7.624e6 7.624e5 3.812e5 2.541e5 1.906e5 \
   1.525e5 7.624e4 #3.812e4
 do
   prefix=C_$C
-  commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope_ref --C=$C --lambda_0=$lambda_ref --goalCFL=$C_CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope_ref --C=$C --lambda_0=$lambda_ref --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
+  echo p_$p/vary_C >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
-C_CFL=10
+CFL=10
 for C in 7.624e8 
 do
   prefix=C_$C
-  commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope_ref --C=$C --lambda_0=$lambda_ref --goalCFL=$C_CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope_ref --C=$C --lambda_0=$lambda_ref --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
+  echo p_$p/vary_C >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
-cd ..
 
-mkdir -p vary_slope
-cd vary_slope
 CFL=400
 for slope in 500 1000 2500 5000 10000 50000
 do
   prefix=slope_$slope
   commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope --C=$C_ref --lambda_0=$lambda_ref --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  echo p_$p/vary_slope >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
 CFL=100
 for slope in 100
 do
   prefix=slope_$slope
   commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope --C=$C_ref --lambda_0=$lambda_ref --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  echo p_$p/vary_slope >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
-cd ..
 
-mkdir -p vary_lambda_0
 CFL=400
-cd vary_lambda_0
 for lambda_0 in 2.500e-1 5.000e-1 1.000e+0 2.000e+0 4.000e+0 8.000e+0 \
   1.600e+1 6.400e+1 1.280e+2
 do
   prefix=lambda_0_$lambda_0
   commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope_ref --C=$C_ref --lambda_0=$lambda_0 --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  echo p_$p/vary_lambda_0 >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
 CFL=100
 for lambda_0 in 3.125e-2 6.250e-2 
 do
   prefix=lambda_0_$lambda_0
   commonArgs="--folder=. --p=$p --A=$A_ref --linearSlope=$slope_ref --C=$C_ref --lambda_0=$lambda_0 --goalCFL=$CFL --initDt=$dt --maxXg=$maxXg --minXg=$minXg"
-  $codeFolder/scripts/runOneCase.bash "$commonArgs" $prefix $codeFolder &
+  echo p_$p/vary_lambda_0 >> $fileName
+  echo $prefix >> $fileName
+  echo $commonArgs >> $fileName
 done
-cd ..
 
-wait
+done
