8f4652ac4942d7bdbe1842c5fc2b17bf5e63165e
xylarstorm@gmail.com <xylarstorm@gmail.com@0952e5f2-6538-abc9-d85b-31e1e9755ba0>
2014-02-07 15:21:47 +0000

    A new way of submitting jobs.
    First, one time run the following script in the root directory containing the directories for each p case:
    > code/scripts/makeExperimentsList.bash
    This will generate experiments.txt, all the info for all "vary" experiments.
    Then, each time you want to submit a new set of jobs, run:
    > code/scripts/makeExperimentSets.bash
    > code/scripts/submitJobs.bash
    The first script will go through the full list of experiments, ignoring those that have finished, and put them in separate sets of 24 in experiments1.txt, etc.
    The second script will submit one job each for experiment sets 1, 2, 3, etc. in experiments1.txt, experiments2.txt, etc.

--- trunk/scripts/submitJobs.bash
+++ trunk/scripts/submitJobs.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-for p in 0.00 0.25 0.50 0.75 1.00
+setNumber=1
+fileName=experiments$setNumber.txt
+while [[ -f $fileName ]]
 do
-  cd p_$p
-  qsub -v p=$p ../code/scripts/jobScriptAllExperiments.bash
-  cd ..
+  qsub -v exptFile=$fileName code/scripts/jobScriptExperimentsFromList.bash
 done
