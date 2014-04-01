ea93feadd2e58024ca55dea968ef41f695308df2
xylarstorm <xylarstorm@gmail.com>
2014-04-01 10:19:25 +0000

    Oops, didn't quite get the indexing bug fixed last time.  Second try.

--- Gunter_fixed_grid/scripts/runMISMIP.py
+++ Gunter_fixed_grid/scripts/runMISMIP.py
@@ -15,14 +15,14 @@ print 'caseFile=', caseFile
 caseList = [line.rstrip('\n') for line in open(caseFile)]
 
 ACount = 17
-for caseIndex in range(ACount):
-  lineIndex=4*(ACount*mismipIndex+caseIndex)
+for caseIndex in range(ACount*mismipIndex,ACount*(mismipIndex+1)):
+  lineIndex=4*caseIndex
   dir = caseList[lineIndex]
   prefix = caseList[lineIndex+1]
   if(os.path.exists("%s/%s_final.pyda"%(dir,prefix))):
     print "case %s/%s already finished."%(dir,prefix)
     continue
-  args = ["python", "code/scripts/runOneCase.py", caseFile, "%i"%(mismipIndex+caseIndex)]  
+  args = ["python", "code/scripts/runOneCase.py", caseFile, "%i"%(caseIndex)]  
   
   print "running case %s/%s."%(dir,prefix)
   status = subprocess.call(args)
