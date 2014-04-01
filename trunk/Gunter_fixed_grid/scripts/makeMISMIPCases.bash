fe04057e1102b899cba271632d13640aaf3b39fb
xylarstorm@gmail.com <xylarstorm@gmail.com@0952e5f2-6538-abc9-d85b-31e1e9755ba0>
2014-04-01 15:56:33 +0000

    Changing dt to be a function of resolution

--- trunk/Gunter_fixed_grid/scripts/makeMISMIPCases.bash
+++ trunk/Gunter_fixed_grid/scripts/makeMISMIPCases.bash
@@ -6,12 +6,12 @@ rm $fileName 2>/dev/null
 slope_ref=778.5
 C_ref=7.624e6
 lambda_ref=2.0
-dt=1e-4
 xc=1.760
 
 
 dxs=( 3.2 1.6 0.8 0.4 0.2 0.1 0.05 )
 Nxs=( 551 1101 2201 4401 8801 17601 35201 )
+dts=( 1e-4 1e-4 3e-4 3e-4 3e-4 3e-4 3e-4 )
 glpStrings=( "" "--useGLP" )
 glpDirs=( nonGLP GLP)
 commonArgs="--folder=. --linearSlope=$slope_ref --C=$C_ref --lambda_0=$lambda_ref --maxSteps=1000000"
@@ -28,6 +28,7 @@ do
       glpstring=${glpStrings[$glpIndex]}
       Nx=${Nxs[$resIndex]}
       dx=${dxs[$resIndex]}
+      dt=${dts[$resIndex]}
       prevResult=none
       for A in 4.6416e-24 2.1544e-24 1.0000e-24 4.6416e-25 2.1544e-25 1.0000e-25 \
         4.6416e-26 2.1544e-26 1.0000e-26
