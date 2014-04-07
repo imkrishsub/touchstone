4f63f61344016edb762a6aac0565f1f86b52768f
xylarstorm@gmail.com <xylarstorm@gmail.com@0952e5f2-6538-abc9-d85b-31e1e9755ba0>
2014-04-07 07:34:48 +0000

    Adding printing of the AIndex (the index 0 to 16 of the A value in the MISMIP experiment) when showing the status of a case

--- trunk/Gunter_fixed_grid/scripts/getCaseStatus.py
+++ trunk/Gunter_fixed_grid/scripts/getCaseStatus.py
@@ -8,22 +8,23 @@ caseList = [line.rstrip('\n') for line in open(caseFile)]
 caseCount = 17*7*5*2
 for caseIndex in range(caseCount):
   mismipIndex = caseIndex/17
+  AIndex = caseIndex-17*mismipIndex
   lineIndex=4*caseIndex
   dir = caseList[lineIndex]
   prefix = caseList[lineIndex+1]
   if(os.path.exists("%s/%s_final.pyda"%(dir,prefix))):
-    print "%i: %s/%s final."%(mismipIndex,dir,prefix)
+    print "%i %i: %s/%s final."%(mismipIndex, AIndex, dir,prefix)
     continue
   logFile="%s/%s_final.log"%(dir,prefix)
   if(os.path.exists(logFile)):
     error=False
     for line in open(logFile):
       if "blew" in line:
-        print "%i: %s/%s failed in final."%(mismipIndex,dir,prefix)
+        print "%i %i: %s/%s failed in final."%(mismipIndex, AIndex, dir,prefix)
         error=True
         break
       if "Error" in line:
-        print "%i: %s/%s failed in final."%(mismipIndex,dir,prefix)
+        print "%i %i: %s/%s failed in final."%(mismipIndex, AIndex, dir,prefix)
         error=True
         break
     if error:
@@ -34,12 +35,12 @@ for caseIndex in range(caseCount):
     error=False
     for line in open(logFile):
       if "blew" in line:
-        print "%i: %s/%s failed in inProgress."%(mismipIndex,dir,prefix)
+        print "%i %i: %s/%s failed in inProgress."%(mismipIndex, AIndex, dir,prefix)
         error=True
         break
       if "Error" in line:
-        print "%i: %s/%s failed in inProgress."%(mismipIndex,dir,prefix)
+        print "%i %i: %s/%s failed in inProgress."%(mismipIndex, AIndex, dir,prefix)
         error=True
         break
     if not error:
-      print "%i: %s/%s inProgress."%(mismipIndex,dir,prefix)
+      print "%i %i: %s/%s inProgress."%(mismipIndex, AIndex, dir,prefix)
